b9fd141bf6891dd23ff430be034a427cacec92a5
StephNereah <stphnadhiambo@gmail.com>
2023-01-11 00:00:08 -0800

    checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper -  checks for uppercase character
+ * @c: is an integer
+ * Return: 1 if uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 41 && c <= 90}
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
