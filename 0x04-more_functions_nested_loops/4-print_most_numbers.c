ad284a18e5cf3b91598d068c804a4c8278d31614
StephNereah <stphnadhiambo@gmail.com>
2023-01-11 01:20:38 -0800

    prints numbers from 0 to 9 except 2, 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,13 +10,14 @@
 
 void print_most_numbers(void)
 {
-	int c;
+	int c = '0';
 
-	for (c = 0; c <= 9; c++)
+	if (c != '2' && c != '4')
 	{
-		if (c != 2 && c != 4)
+		while (c <= '9')
 		{
 			_putchar(c);
+			c++;
 		}
 	}
 	_putchar('\n');
