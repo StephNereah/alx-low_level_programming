010ffde79e80a893be13a98d3e016b16390e6502
StephNereah <stphnadhiambo@gmail.com>
2023-10-13 16:34:20 -0700

    3. 11

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * clear_bit - sets the value of a bit to 0 at a given index
+ * set_bit - sets the value of a bit to 0 at a given index
  *
  * @index: is the index, starting from 0 of the bit you want to set
- * @*n: is a pointer 
+ * @n: is an int
  *
  * Return: 1 if it worked, or -1 if an error occurred
  *
