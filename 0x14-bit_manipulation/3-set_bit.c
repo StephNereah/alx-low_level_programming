a8164cedb3d8a3f06f10e0a082ada096139ecc29
StephNereah <stphnadhiambo@gmail.com>
2023-10-13 16:26:49 -0700

    3. 11

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ *
+ * @index: is the index, starting from 0 of the bit you want to set
+ * @*n: is a pointer 
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ *
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int m = 1 << index;
+
+	if (index <= (sizeof(n) * 8) - 1)
+	{
+		*n |= m;
+
+		return (1);
+	}
+
+	return (-1);
+}
